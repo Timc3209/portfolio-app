aab38b6efd00ba0cf800c6413ff508238aaf55c6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-21 00:30:49 +0000

    Deploy Timc3209/portfolio-app to github.com/Timc3209/portfolio-app.git:gh-pages

---
+++ precache-manifest.0327bb55ac5ef0413566c0a5a2b589dd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "358fa86281793dfae7cf798f036d610f",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "aaa1fd6ccdbb3a8be2ee",
+    "url": "/portfolio-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "de125a17612bba5d33dc",
+    "url": "/portfolio-app/static/css/main.d1beb9dd.chunk.css"
+  },
+  {
+    "revision": "aaa1fd6ccdbb3a8be2ee",
+    "url": "/portfolio-app/static/js/2.92acc9ff.chunk.js"
+  },
+  {
+    "revision": "a986d777e7ab357b3f79ca1eede38fb5",
+    "url": "/portfolio-app/static/js/2.92acc9ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de125a17612bba5d33dc",
+    "url": "/portfolio-app/static/js/main.a6798143.chunk.js"
+  },
+  {
+    "revision": "ed84828383915bc7a56f",
+    "url": "/portfolio-app/static/js/runtime-main.44fb9206.js"
+  },
+  {
+    "revision": "48008788c713c260a815852b9278ea3b",
+    "url": "/portfolio-app/static/media/recipeapp-sq.48008788.png"
+  },
+  {
+    "revision": "c09391a573543d9cff9c9aa0515c0195",
+    "url": "/portfolio-app/static/media/todoapp.c09391a5.png"
+  }
+]);
